e42dd26dfa39c0a273e56cc882c01722b3ce4152
mlutegar <mlutegar@gmail.com>
2024-10-16 16:05:08 -0300

    feat: adicionado container

--- src/main/java/com/br/patientecare/config/WebConfig.java
+++ src/main/java/com/br/patientecare/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://mlutegar.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
